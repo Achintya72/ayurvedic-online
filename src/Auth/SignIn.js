d1942bd1a6b3e63082e65d40804b24e69e73e88d
Abhi Agrawal <abhiaga@microsoft.com>
2020-10-08 11:11:36 -0700

    Working branch

--- src/Auth/SignIn.js
+++ src/Auth/SignIn.js
@@ -1,39 +1,19 @@
-import React, {Component} from 'react'
-import './App.css'
-import firebase from "firebase"
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+import React from 'react';
+//import {FirebaseContext} from '../Utils';
+//import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 
-
-// Configure Firebase.
-const config = {
-  apiKey: "AIzaSyAaxyFllQke803zcefgcZE34rbxw7tY4G0",
-  authDomain: "ayurvedic-online.firebaseapp.com",
-  // ...
-};
-firebase.initializeApp(config);
 
 // Configure FirebaseUI.
-const uiConfig = {
-  // Popup signin flow rather than redirect flow.
-  signInFlow: 'popup',
-  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: '/signedIn',
-  // We will display Google and Facebook as auth providers.
-  signInOptions: [
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID
-  ]
-};
 
-class SignIn extends React.Component {
-  render() {
+function SignIn() {
+   // const firebase = useContext(FirebaseContext);
     return (
       <div>
         <h1>My App</h1>
         <p>Please sign-in:</p>
-        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
+        {/* <StyledFirebaseAuth uiConfig={firebase.uiConfig} firebaseAuth={firebase.auth}/> */}
       </div>
     );
-  }
 }
+
+export default SignIn
