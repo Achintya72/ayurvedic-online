c8cc0763e8f95b857d9217d689493b95ab68dc2f
Achintya27 <achintya.agrawal.90@gmail.com>
2020-10-10 19:10:43 -0700

    Added Fonts, and made the Nav using MUI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
 
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Navigation from './Navigation';
 import Landing from './Landing';
-import {SignIn, SignUp} from './Auth';
+import { SignIn, SignUp } from './Auth';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#151515'
+        },
+        secondary: {
+            main: '#FFFFFC'
+        },
+        success: {
+            main: '#768948'
+        }
+
+    },
+    typography: {
+        body1: {
+            fontFamily: [
+                'Montserrat'
+            ],
+            fontSize: '17pt'
+        },
+        button: {
+            fontFamily: [
+                'Hind'
+            ],
+            fontWeight: '400',
+            fontSize: '12pt'
+        }
+    }
+})
 
 function App() {
-    return(
-        <div>
-            <Navigation />
-            <br/>
-            <Switch>
-                <Route exact path='/' component={Landing} />
-                <Route path='/signin' component={SignIn} />
-            </Switch>
-        </div>
+    return (
+        <ThemeProvider theme={theme}>
+            <div>
+                <Navigation />
+                <br />
+                <Switch>
+                    <Route exact path='/' component={Landing} />
+                    <Route path='/signin' component={SignIn} />
+                </Switch>
+            </div>
+        </ThemeProvider>
     )
 }
 
--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1
+    },
+    title: {
+        flexGrow: 1
+    },
+    links: {
+        textDecoration: 'none',
+        color: 'secondary'
+    }
+}))
+
 
 export default function Navigation() {
+    const classes = useStyles();
     return (
         <>
-            <Link to='/'>Home</Link>
-            <Link to='/signin'>Sign In</Link>
+            <AppBar position="sticky" color="primary">
+                <Toolbar>
+                    <Typography className={classes.title}>Ayurvedic Online</Typography>
+                    <Button color="secondary" href="/">Home</Button>
+                    <Button color="secondary" href="/reviews">Reviews</Button>
+                    <Button color="secondary" href="/signin">Sign In</Button>
+                </Toolbar>
+
+            </AppBar>
         </>
     )
 }
